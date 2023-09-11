5db2a7425d2a020d6b1a569d7acb73bd5ae64665
dopecello <williamlocke.cello@gmail.com>
2023-09-11 17:06:38 -0400

    Integrating Jackson software for JSON binding

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,19 +1,24 @@
 package com.pluralsight.courseinfo.cli;
 
 import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
+import com.pluralsight.courseinfo.cli.service.PluralSightCourse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class CourseRetriever {
     private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String... args) {
         LOG.info("CourseRetriever is starting!");
         if (args.length == 0) {
             LOG.warn("Please provide an author name as a first argument.");
             return;
         }
-        try { retrieveCourses(args[0]); }
-        catch (Exception e) {
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
             LOG.error("An unexpected error has occurred.", e);
         }
     }
@@ -21,7 +26,7 @@ public class CourseRetriever {
     private static void retrieveCourses(String authorId) {
         LOG.info("Retrieving courses for author '{}'", authorId);
         CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
-        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
-        LOG.info("Retrieved the following courses {}", coursesToStore);
+        List<PluralSightCourse> coursesToStore = courseRetrievalService.getCoursesFor(authorId);
+        LOG.info("Retrieved the following {} courses {}", coursesToStore.size(), coursesToStore);
     }
 }
